f66979caec195d929c917c734febb69a97ded103
Demetrius <Demetrius>
2015-06-22 23:49:15 +0000

    Revert "Faster gate detection."
    This reverts commit 51c39851ccc3a69aa7d409e3751d83ca629857f1.

--- seeGate/api_test.py
+++ seeGate/api_test.py
@@ -5,26 +5,29 @@ from ospery_diver.util import *
 
 if __name__ == '__main__':
 
-	cam = Camera(cv2)
-	display = Display(cv2)
-	executor = Executor(cv2, np)
+    cam = Camera(cv2)
+    display = Display(cv2)
+    executor = Executor(cv2, np)
 
-	while( True ):
+    img_num = 0
+    while( True ):
+        img_num += 1
+        frame = cam.getFrame()
+        if img_num % 10 == 0:
+            cv2.imwrite( "./image-"+str(img_num)+ ".jpg", frame );
+        point = executor.run(tasks.gateDetector, frame, None)
 
-		frame = cam.getFrame()
+        if point is not None:
+            #draw a cross hair
+            cv2.circle(frame, (point.getX() ,point.getY()), 40, (0,0,255), 5)
 
-		point = executor.run(tasks.gateDetector, frame, None)
+         #rects = executor.run(tasks.findBoundingRectsByColor, frame, ORANGE)
 
-		if point is not None:
-			#draw a cross hair
-			cv2.circle(frame, (point.getX() ,point.getY()), 40, (0,0,255), 5)
 
- 		#rects = executor.run(tasks.findBoundingRectsByColor, frame, ORANGE)
+        #for rect in rects:
+        #    display.drawContour(frame, rect, RED, 2)
 
-		#for rect in rects:
-		#	display.drawContour(frame, rect, RED, 2)
+        if (display.show(frame, "output")==-1):
+            break
 
-		if (display.show(frame, "output")==-1):
-			break
-
-	display.destroy()
+    display.destroy()
